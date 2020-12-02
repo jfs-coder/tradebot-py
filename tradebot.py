b36b167e13aeb0ff79baa2140dc680f058556cbe
jfs-coder <jfs-coder@protonmail.com>
2020-12-02 03:40:47 +0000

    initial skeleton file

---
+++ tradebot.py
@@ -0,0 +1,30 @@
+# TradeBot Skeleton / Pseudocode
+
+# Create some variables such as
+startCapial = 1000
+tranches = 5
+
+# def GetBalances()
+
+# def GetAccountInfos()
+
+# def GetOrders()
+
+# def GetOrderStatus($OrderID)
+
+# def SetLimitSellOrder($amount, $price)
+
+# def SetLimitBuyOrder ($amount, $price)
+
+# def GetOrderHistory()
+
+# def GetActiveOrders()
+
+# def SetOrderCancel($orderid)
+
+# def SetPayloadEncoder()
+
+# def SetCancelAllOrders()
+
+# def SetReloadOrders()
+
